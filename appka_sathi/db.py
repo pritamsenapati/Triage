81abae2c15b5c125c31a0c328f8f5ed52497e1bc
hari <pritamsenapati2004@gmail.com>
2026-04-28 00:01:09 +0530

    Fixed

--- appka_sathi/db.py
+++ appka_sathi/db.py
@@ -1,10 +1,21 @@
+# import mysql.connector
+
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="yourpassword",
+#         database="healthcare_db"
+#     )
+
 import mysql.connector
+import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="yourpassword",
-        database="healthcare_db"
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT"))
     )
-
